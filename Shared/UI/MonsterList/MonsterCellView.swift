32a84d4352586465f65f67fcb58a86a4a0d13c22
uhooi <the_uhooi@yahoo.co.jp>
2022-07-12 00:13:39 +0900

    Add MonsterCellView

---
+++ Shared/UI/MonsterList/MonsterCellView.swift
@@ -0,0 +1,54 @@
+//
+//  MonsterCellView.swift
+//  UhooiPicBookSwiftUIPlayground (iOS)
+//
+//  Created by uhooi on 2022/07/11.
+//
+
+import SwiftUI
+
+struct MonsterCellView: View {
+    var iconName: String
+    var name: String
+    
+    var body: some View {
+        HStack(spacing: 32) {
+            Image(iconName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 68, height: 68)
+            Text(name)
+                .font(.title)
+            Spacer()
+        }
+        .padding(16)
+        .background(
+            Color(.systemBackground)
+                .cornerRadius(3)
+                .elevate(elevation: 1)
+        )
+        .padding(8)
+        .frame(maxWidth: .infinity, minHeight: 116)
+    }
+}
+
+private extension View {
+    func elevate(elevation: CGFloat) -> some View {
+        self.shadow(
+            color: .init(white: 0, opacity: 0.24),
+            radius: elevation,
+            x: 0,
+            y: elevation
+        )
+    }
+}
+
+struct MonsterCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 12) {
+            MonsterCellView(iconName: "uhooi", name: "uhooi")
+            MonsterCellView(iconName: "uhooi", name: "とてつもなく長い名前のモンスター")
+        }
+        .padding(.horizontal, 16)
+    }
+}
--- Shared/UhooiPicBookSwiftUIPlaygroundApp.swift
+++ Shared/UhooiPicBookSwiftUIPlaygroundApp.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct UhooiPicBookSwiftUIPlaygroundApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            VStack(spacing: 12) {
+                MonsterCellView(iconName: "uhooi", name: "uhooi")
+                MonsterCellView(iconName: "uhooi", name: "とてつもなく長い名前のモンスター")
+            }
+            .padding(.horizontal, 16)
         }
     }
 }
