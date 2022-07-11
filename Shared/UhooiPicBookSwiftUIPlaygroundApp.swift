fad74dea8ca50df4f139909323c41c8f8254e99f
uhooi <the_uhooi@yahoo.co.jp>
2022-07-11 21:29:45 +0900

    Create project

---
+++ Shared/UhooiPicBookSwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  UhooiPicBookSwiftUIPlaygroundApp.swift
+//  Shared
+//
+//  Created by uhooi on 11.7.2022.
+//
+
+import SwiftUI
+
+@main
+struct UhooiPicBookSwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
